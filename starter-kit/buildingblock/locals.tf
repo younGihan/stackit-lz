c6a23ff5d53d2f843b079966dad27266bb42b8ac
Young-Hwan <yhkim@meshcloud.io>
2026-08-10 11:35:41 +0200

    custom config for test

--- starter-kit/buildingblock/locals.tf
+++ starter-kit/buildingblock/locals.tf
@@ -2,8 +2,8 @@ locals {
   stackit_platform = one(data.meshstack_platforms.stackit.platforms)
 
   landing_zone_names = {
-    qa   = "qa"
-    prod = "prod"
+    qa   = "stackit-dev"
+    prod = "stackit-prod"
   }
 
   qa_landing_zone   = one(data.meshstack_landingzones.qa.landing_zones)
